ba1b0a01756a314b896681a6d133656d47c8b1a0
YALLICO OWNER <yallico2024@gmail.com>
2026-06-18 09:13:55 -0500

    Actualizar tools-whatmusic.js

--- plugins/tools-whatmusic.js
+++ plugins/tools-whatmusic.js
@@ -13,7 +13,7 @@ let handler = async (m, { conn, usedPrefix, command }) => {
   let { status, metadata } = await acr.identify(buffer)
   if (status.code !== 0) throw status.msg 
   let { title, artists, album, genres, release_date } = metadata.music[0]
-  let txt = '╭─⬣「 *🐉 Whatmusic Gohan 🌀* 」⬣\n'
+  let txt = '╭─⬣「 *🐉 Whatmusic 🌀* 」⬣\n'
       txt += `│  ≡◦ *Titulo ∙* ${title}${artists ? `\n│  ≡◦ *Artista ∙* ${artists.map(v => v.name).join(', ')}` : ''}`
       txt += `${album ? `\n│  ≡◦ *Album ∙* ${album.name}` : ''}${genres ? `\n│  ≡◦ *Genero ∙* ${genres.map(v => v.name).join(', ')}` : ''}\n`
       txt += `│  ≡◦ *Fecha de lanzamiento ∙* ${release_date}\n`
@@ -21,7 +21,7 @@ let handler = async (m, { conn, usedPrefix, command }) => {
      conn.reply(m.chat, txt, m)
   } else return conn.reply(m.chat, `Etiqueta un audio o video de poca duración con el comando *${usedPrefix + command}* para ver que música contiene.`, m)
 }
-handler.help = ['whatmusic <audio/video>']
+handler.help = ['shazam <audio/video>']
 handler.tags = ['tools']
 handler.command = ['shazam', 'whatmusic']
 handler.register = false
